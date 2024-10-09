cd2ac5d86f01b5c74c61635f644288d878a210a4
zidan leo permana <zidanleopermana012@gmail.com>
2024-10-10 00:25:44 +0700

    Add early new project dropup form

---
+++ src/modules/addProjectStyle.js
@@ -0,0 +1,15 @@
+export default function () {
+    const dropbtn = document.querySelector(".dropbtn")
+    const dropUpContent = document.querySelector(".dropup-content")
+    let isDisplayed = false;
+
+    dropbtn.addEventListener('click', e => {
+        if (isDisplayed) {
+            dropUpContent.style['display'] = 'block';
+            isDisplayed = false
+        } else {
+            dropUpContent.style['display'] = 'none';
+            isDisplayed = true
+        }
+    })
+}
